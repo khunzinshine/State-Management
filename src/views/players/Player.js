8a89cd88a4ab5bf97038ad386b87f3b196fd6eda
khunzinshine <khunzinshine@gmail.com>
2023-10-09 06:14:37 +0630

    fix: player list

--- src/views/players/Player.js
+++ src/views/players/Player.js
@@ -1,25 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import { ApiEndPoints } from '../../constants/ApiEndPoints';
-import { Button } from 'primereact/button';
-import Layout from '../../components/layout/Layout';
-import PlayerCard from '../../components/PlayerCard';
-import { ScrollTop } from 'primereact/scrolltop';
-import { fetchPlayerList } from '../../features/players/playerAPI';
+import { ApiEndPoints } from "../../constants/ApiEndPoints";
+import { Button } from "primereact/button";
+import Layout from "../../components/layout/Layout";
+import PlayerCard from "../../components/PlayerCard";
+import { ScrollTop } from "primereact/scrolltop";
+import { fetchPlayerList } from "../../features/players/playerAPI";
 
 const Player = () => {
   const dispatch = useDispatch();
   const [limit, setLimit] = useState(10);
   const {
-    data: { data },
-  } = useSelector((state) => state.player);
+    data: { data }
+  } = useSelector(state => state.player);
   const { loading } = useSelector(({ system }) => system);
 
   const fetchData = () => {
     dispatch(
       fetchPlayerList({
-        url: `${ApiEndPoints.playerList}?page=${1}&&per_page=${limit}`,
+        url: `${ApiEndPoints.playerList}?page=${1}&&per_page=${limit}`
       })
     );
   };
@@ -30,32 +30,32 @@ const Player = () => {
   }, [limit]);
 
   const loadMore = () => {
-    setLimit((preState) => preState + 10);
+    setLimit(preState => preState + 10);
   };
 
   return (
     <Layout>
-      <h2 className='flex justify-content-center mb-4 mt-4'>Player List</h2>
-      {data?.data.map((player) => (
+      <h2 className="flex justify-content-center mb-4 mt-4">Player List</h2>
+      {data?.data.map(player => (
         <PlayerCard
           key={player.id}
           title={`${player.first_name} ${player.last_name}`}
-          position={player?.position || 'N/A'}
-          city={player?.team?.city || 'N/A'}
-          division={player?.team?.division || 'N/A'}
+          position={player?.position || "N/A"}
+          city={player?.team?.city || "N/A"}
+          division={player?.team?.division || "N/A"}
         />
       ))}
       <Button
-        className='mt-2 mb-5 w-full'
+        className="mt-2 mb-5 w-full"
         loading={loading}
-        label='Load More'
+        label="Load More"
         onClick={loadMore}
       />
       <ScrollTop
-      // target='parent'
-      // threshold={100}
-      // className='w-2rem h-2rem border-round bg-primary'
-      // icon='pi pi-arrow-up text-base'
+        target="parent"
+        threshold={100}
+        className="w-2rem h-2rem border-round bg-primary"
+        icon="pi pi-arrow-up text-base"
       />
     </Layout>
   );
