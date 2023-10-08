c534e1f8349dac9ba61d68edaea6234f219106c2
khunzinshine <khunzinshine@gmail.com>
2023-10-09 06:09:09 +0630

    fix: AuthContext

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,40 +1,40 @@
-import { createContext, useEffect, useReducer } from 'react';
+import { createContext, useEffect, useReducer } from "react";
 
 const INITIAL_STATE = {
-  user: JSON.parse(localStorage.getItem('user')) || null,
-  token: JSON.parse(localStorage.getItem('token')) || null,
+  user: JSON.parse(localStorage.getItem("user")) || null,
+  token: JSON.parse(localStorage.getItem("token")) || null,
   loading: false,
-  error: null,
+  error: null
 };
 
 export const AuthContext = createContext(INITIAL_STATE);
 
 const AuthReducer = (state, action) => {
   switch (action.type) {
-    case 'LOGIN_START':
+    case "LOGIN_START":
       return {
         user: null,
         loading: true,
-        error: null,
+        error: null
       };
-    case 'LOGIN_SUCCESS':
+    case "LOGIN_SUCCESS":
       return {
         user: action.payload,
         token: action.payload,
         loading: false,
-        error: null,
+        error: null
       };
-    case 'LOGIN_FAILURE':
+    case "LOGIN_FAILURE":
       return {
         user: null,
         loading: false,
-        error: action.payload,
+        error: action.payload
       };
-    case 'LOGOUT':
+    case "LOGOUT":
       return {
         user: null,
         loading: false,
-        error: null,
+        error: null
       };
     default:
       return state;
@@ -45,20 +45,18 @@ export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
 
   useEffect(() => {
-    localStorage.setItem('user', JSON.stringify(state.user));
-    localStorage.setItem('token', JSON.stringify(state.token));
+    localStorage.setItem("user", JSON.stringify(state.user));
+    localStorage.setItem("token", JSON.stringify(state.token));
   }, [state.user, state.token]);
 
   const logout = () => {
-    localStorage.removeItem('token');
-    localStorage.removeItem('user');
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
   };
 
   const isExpired = () => {
     if (
-      state.token &&
-      state.token.token &&
-      state.token.expiredAt &&
+      state?.token?.expiredAt &&
       new Date().getTime() > new Date(state.token.expiredAt).getTime()
     ) {
       logout();
@@ -76,7 +74,7 @@ export const AuthContextProvider = ({ children }) => {
         error: state.error,
         dispatch,
         logout,
-        isExpired,
+        isExpired
       }}
     >
       {children}
