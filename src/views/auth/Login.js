b35c363d03f57ea28d6e51d1bb47e20aa4ec1cdc
khunzinshine <khunzinshine@gmail.com>
2023-10-09 06:10:18 +0630

    fix: login form

--- src/views/auth/Login.js
+++ src/views/auth/Login.js
@@ -1,13 +1,13 @@
-import * as Yup from 'yup';
+import * as Yup from "yup";
 
-import { Form, Formik } from 'formik';
-import React, { useContext, useRef } from 'react';
+import { Form, Formik } from "formik";
+import React, { useContext, useRef } from "react";
 
-import { AuthContext } from '../../context/AuthContext';
-import { Button } from 'primereact/button';
-import { InputText } from 'primereact/inputtext';
-import { Toast } from 'primereact/toast';
-import { useNavigate } from 'react-router-dom';
+import { AuthContext } from "../../context/AuthContext";
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
+import { Toast } from "primereact/toast";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const toast = useRef(null);
@@ -26,109 +26,107 @@ const Login = () => {
       values.password === defaultPassword
     ) {
       dispatch({
-        type: 'LOGIN_START',
+        type: "LOGIN_START"
       });
 
       let authInfo = {
-        token: {
-          token: defaultToken,
-          expiredAt: new Date(Date.now() + 24 * 60 * 1000),
-        },
-        user: values.username,
+        token: defaultToken,
+        expiredAt: new Date(Date.now() + 24 * 60 * 1000),
+        user: values.username
       };
 
       dispatch({
-        type: 'LOGIN_SUCCESS',
-        payload: authInfo,
+        type: "LOGIN_SUCCESS",
+        payload: authInfo
       });
 
-      navigate('/team/list');
+      navigate("/team/list");
     } else {
       toast.current.show({
-        severity: 'error',
-        summary: 'Error',
-        detail: 'Username and Password Incorrect',
+        severity: "error",
+        summary: "Error",
+        detail: "Username and Password Incorrect"
       });
     }
   };
 
   const SigninSchema = Yup.object().shape({
     username: Yup.string()
-      .min(2, 'Too Short!')
-      .max(50, 'Too Long!')
-      .required('Required'),
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
     password: Yup.string()
-      .min(2, 'Too Short!')
-      .max(50, 'Too Long!')
-      .required('Required'),
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required")
   });
 
   return (
-    <div style={{ marginTop: '10rem' }}>
+    <div style={{ marginTop: "10rem" }}>
       <Toast ref={toast} />
       <Formik
         initialValues={{
-          username: '',
-          password: '',
+          username: "",
+          password: ""
         }}
         validationSchema={SigninSchema}
         onSubmit={onSubmit}
       >
         {({ values, errors, touched, handleChange }) => (
           <Form>
-            <div className='flex align-items-center justify-content-center'>
-              <div className='surface-card p-4 shadow-2 border-round w-full lg:w-6'>
-                <div className='text-center mb-5'>
-                  <div className='text-900 text-3xl font-medium mb-3 mt-5'>
+            <div className="flex align-items-center justify-content-center">
+              <div className="surface-card p-4 shadow-2 border-round w-full lg:w-6">
+                <div className="text-center mb-5">
+                  <div className="text-900 text-3xl font-medium mb-3 mt-5">
                     Signin to App
                   </div>
                 </div>
 
                 <div>
                   <label
-                    htmlFor='email'
-                    className='block text-900 font-medium mb-2'
+                    htmlFor="email"
+                    className="block text-900 font-medium mb-2"
                   >
                     Username
                   </label>
                   <InputText
-                    id='username'
-                    type='text'
-                    autoComplete='username'
+                    id="username"
+                    type="text"
+                    autoComplete="username"
                     value={values.username}
                     onChange={handleChange}
                     validateOnly={values.password && !errors.password}
-                    placeholder='Username'
-                    className='w-full mb-3'
+                    placeholder="Username"
+                    className="w-full mb-3"
                   />
                   {errors.username && touched.username ? (
                     <div>{errors.username}</div>
                   ) : null}
                   <label
-                    htmlFor='password'
-                    className='block text-900 font-medium mb-2'
+                    htmlFor="password"
+                    className="block text-900 font-medium mb-2"
                   >
                     Password
                   </label>
                   <InputText
-                    id='password'
-                    type='password'
-                    autoComplete='password'
+                    id="password"
+                    type="password"
+                    autoComplete="password"
                     validateOnly={values.password && !errors.password}
                     value={values.password}
                     onChange={handleChange}
-                    placeholder='Password'
-                    className='w-full mb-3'
+                    placeholder="Password"
+                    className="w-full mb-3"
                   />
                   {errors.password && touched.password ? (
                     <div>{errors.password}</div>
                   ) : null}
-                  <div className='flex justify-content-center mt-4 mb-5'>
+                  <div className="flex justify-content-center mt-4 mb-5">
                     <Button
-                      label='Sign In'
-                      type='submit'
+                      label="Sign In"
+                      type="submit"
                       loading={loading}
-                      className='w-full'
+                      className="w-full"
                     />
                   </div>
                 </div>
