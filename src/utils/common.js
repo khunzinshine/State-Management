39b333c702cd3d90a8bcca8857856f49b98ef008
khunzinshine <khunzinshine@gmail.com>
2023-10-09 06:09:52 +0630

    add: common utils func

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,7 +1,7 @@
-export const modifiedArrays = (data) => {
+export const modifiedArrays = data => {
   return (
     data &&
-    data.map((d) => {
+    data?.map(d => {
       return Object.assign({ key: d.id, name: d.first_name, code: d.position });
     })
   );
