241c5f829f0177389d30f93925b23fa525d3c2fc
khunzinshine <khunzinshine@gmail.com>
2023-10-09 06:09:21 +0630

    fix: Team form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,55 +1,55 @@
-import * as Yup from 'yup';
+import * as Yup from "yup";
 
-import { Form, Formik } from 'formik';
+import { Form, Formik } from "formik";
 
-import { Button } from 'primereact/button';
-import { InputText } from 'primereact/inputtext';
-import { MultiSelect } from 'primereact/multiselect';
-import React from 'react';
-import { modifiedArrays } from '../utils/common';
-import { useSelector } from 'react-redux';
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
+import { MultiSelect } from "primereact/multiselect";
+import React from "react";
+import { modifiedArrays } from "../utils/common";
+import { useSelector } from "react-redux";
 
-const TeamForm = ({ onSubmit, players }) => {
-  const { data, detail } = useSelector((state) => state.team);
+const TeamForm = ({ onSubmit, players, isCreate }) => {
+  const { data, detail } = useSelector(state => state.team);
 
-  let teamData = data.map((d) => d.data).filter((p) => p.player);
-  let teamPlayers = teamData.map((t) => t.player);
-  const filteredPlayers = players?.data?.filter((player) =>
-    teamPlayers.map(
-      (each) => !each.some((sub) => sub.name === player.first_name)
-    )
+  let options = modifiedArrays(players.data);
+  // the players that are already added to the team
+  const playersList = data.map(player => player.data.player).flat();
+  const playersAddedToTeam = new Set(playersList.map(item => item.key));
+  // remove the players that are already added to the team from the options
+  const validPlayersToAddTeam = options.filter(
+    item => !playersAddedToTeam.has(item.key)
   );
-  let options = modifiedArrays(filteredPlayers);
 
   const TeamSchema = Yup.object().shape({
     name: Yup.string()
-      .min(2, 'Too Short!')
-      .max(50, 'Too Long!')
-      .required('Required'),
-    player: Yup.array().required('Required'),
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    player: Yup.array().required("Required"),
     region: Yup.string()
-      .min(2, 'Too Short!')
-      .max(50, 'Too Long!')
-      .required('Required'),
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
     country: Yup.string()
-      .min(2, 'Too Short!')
-      .max(50, 'Too Long!')
-      .required('Required'),
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required")
   });
 
-  const playerTemplate = (option) => {
+  const playerTemplate = option => {
     return (
-      <div className='flex align-items-center'>
+      <div key={option.id} className="flex align-items-center">
         <div key={option.id}>{option.name}</div>
       </div>
     );
   };
-  const panelFooterTemplate = (value) => {
+  const panelFooterTemplate = value => {
     const length = value ? value.length : 0;
 
     return (
-      <div className='py-2 px-3'>
-        <b>{length}</b> player{length > 1 ? 's' : ''} selected.
+      <div className="py-2 px-3">
+        <b>{length}</b> player{length > 1 ? "s" : ""} selected.
       </div>
     );
   };
@@ -57,97 +57,97 @@ const TeamForm = ({ onSubmit, players }) => {
   return (
     <Formik
       initialValues={{
-        name: detail ? detail.data?.name : '',
+        name: detail ? detail.data?.name : "",
         player: detail ? detail.data?.player : [],
-        region: detail ? detail.data?.region : '',
-        country: detail ? detail.data?.country : '',
+        region: detail ? detail.data?.region : "",
+        country: detail ? detail.data?.country : ""
       }}
       validationSchema={TeamSchema}
       onSubmit={onSubmit}
     >
       {({ values, errors, touched, handleChange }) => (
         <Form>
-          <div className='m-20 align-items-center justify-content-center'>
+          <div className="m-20 align-items-center justify-content-center">
             <div>
-              <label htmlFor='name' className='block text-900 font-medium mb-2'>
+              <label htmlFor="name" className="block text-900 font-medium mb-2">
                 Username
               </label>
               <InputText
-                id='name'
-                type='text'
-                autoComplete='name'
+                id="name"
+                type="text"
+                autoComplete="name"
                 value={values.name}
                 onChange={handleChange}
                 validateOnly={values.name && !errors.name}
-                placeholder='Name'
-                className='w-full mb-3'
+                placeholder="Name"
+                className="w-full mb-3"
               />
               {errors.name && touched.name ? <div>{errors.name}</div> : null}
               <label
-                htmlFor='player'
-                className='block text-900 font-medium mb-2'
+                htmlFor="player"
+                className="block text-900 font-medium mb-2"
               >
                 Player
               </label>
               <MultiSelect
-                id='player'
+                id="player"
                 value={values.player}
-                options={options}
+                options={isCreate ? validPlayersToAddTeam : options}
                 onChange={handleChange}
-                optionLabel='name'
-                placeholder='Select Player'
+                optionLabel="name"
+                placeholder="Select Player"
                 itemTemplate={playerTemplate}
                 panelFooterTemplate={panelFooterTemplate(values.player)}
-                className='w-full mb-3'
-                display='chip'
+                className="w-full mb-3"
+                display="chip"
               />
               {errors.player && touched.player ? (
                 <div>{errors.player}</div>
               ) : null}
 
               <label
-                htmlFor='region'
-                className='block text-900 font-medium mb-2'
+                htmlFor="region"
+                className="block text-900 font-medium mb-2"
               >
                 Region
               </label>
               <InputText
-                id='region'
-                type='text'
-                autoComplete='region'
+                id="region"
+                type="text"
+                autoComplete="region"
                 value={values.region}
                 onChange={handleChange}
                 validateOnly={values.password && !errors.password}
-                placeholder='Region'
-                className='w-full mb-3'
+                placeholder="Region"
+                className="w-full mb-3"
               />
               {errors.region && touched.region ? (
                 <div>{errors.region}</div>
               ) : null}
 
               <label
-                htmlFor='country'
-                className='block text-900 font-medium mb-2'
+                htmlFor="country"
+                className="block text-900 font-medium mb-2"
               >
                 Country
               </label>
               <InputText
-                id='country'
-                type='text'
-                autoComplete='country'
+                id="country"
+                type="text"
+                autoComplete="country"
                 value={values.country}
                 onChange={handleChange}
                 validateOnly={values.password && !errors.password}
-                placeholder='Country'
-                className='w-full mb-3'
+                placeholder="Country"
+                className="w-full mb-3"
               />
               {errors.country && touched.country ? (
                 <div>{errors.country}</div>
               ) : null}
             </div>
           </div>
-          <div className='flex justify-content-end'>
-            <Button icon='pi pi-check' label='Submit' type='submit' />
+          <div className="flex justify-content-end">
+            <Button icon="pi pi-check" label="Submit" type="submit" />
           </div>
         </Form>
       )}
